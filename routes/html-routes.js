793b2078c5102088df378f72f5a633a278054e61
ginnac <ginnacampbell1990@gmail.com>
2019-09-22 16:28:56 -0600

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,20 @@
-var db = require("../models");
+    var path = require("path");
 
     module.exports = function(app) {
 
+        // index route loads view.html
+        app.get("/", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        // cms route loads cms.html
+        app.get("/portfolio", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+        });
+
+        // blog route loads blog.html
+        app.get("/contact", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/blog.html"));
+        });
         
     }
